48c85621061cc24ebbd45510614c3b5426cbf2cc
wildbook7 <wildbook7@gmail.com>
2024-11-29 17:38:46 +0900

    スナップショットテスト

---
+++ frontend/src/components/SettlementList/SettlementList.test.tsx
@@ -0,0 +1,14 @@
+import { Settlement } from "../../type";
+import SettlementList from "./SettlementList";
+import { render } from "@testing-library/react";
+
+describe("SettlementList", () => {
+  test("Snapshot test", () => {
+    const settlements: Settlement[] = [
+      { from: "二郎", to: "一郎", amount: 1000 },
+    ];
+
+    const { container } = render(<SettlementList settlements={settlements} />);
+    expect(container).toMatchSnapshot();
+  });
+});
