15880e5d5179eca71d9fb2619af857f52f2342fb
wildbook7 <wildbook7@gmail.com>
2024-11-29 17:27:38 +0900

    コンポーネントを Storybook へ追加

---
+++ frontend/src/components/CreateGroupForm/CreateGroupForm.stories.tsx
@@ -0,0 +1,19 @@
+import CreateGroupForm from "./CreateGroupForm";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "CreateGroupForm",
+  component: CreateGroupForm,
+} as Meta<typeof CreateGroupForm>;
+
+export default meta;
+
+type Story = StoryObj<typeof CreateGroupForm>;
+
+export const Default: Story = {
+  args: {
+    onSubmit: async () => {
+      return new Promise((resolve) => setTimeout(() => resolve(), 1000));
+    },
+  },
+};
